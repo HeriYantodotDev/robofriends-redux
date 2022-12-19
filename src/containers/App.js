b4e2d8e43a6ac3242548200d3971764b719e53e0
HeriYantodotDev <heri@Tukang_Logika>
2022-12-19 14:28:34 +0700

    using redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {connect} from 'react-redux'
 import Cardlist from "../components/Cardlist";
 import generateRobots from "../robots";
 import SearchBox from '../components/SearchBox';
@@ -6,6 +7,8 @@ import Scroll from "../components/Scroll";
 import './App.css'
 import InputBox from "../components/InputBox";
 import ErrorBoundry from "../components/ErrorBoundry";
+import { requestRobots, setSearchField } from "../actions";
+
 
 //generate how much robots:
 
@@ -19,13 +22,31 @@ import ErrorBoundry from "../components/ErrorBoundry";
 //     );
 // }
 
+const mapStateToProps = (state) => {
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error : state.requestRobots.error
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        clearSearchField : (event) => dispatch(setSearchField(event.target.parentElement.parentElement.children[2].children[0].value)),
+        onRequestRobots: () => dispatch(requestRobots())
+    }
+}
+
 class App extends Component {
     constructor () {
         super();
         this.state = {
             totalRobot : 0,
-            robots: [],
-            searchfield: ''    
+            robotsGenerate: [],
+            //we don't need searchfield since we already have it on Redux
+            // searchfield: ''    
         }
     }
 
@@ -35,7 +56,11 @@ componentDidMount() {
     //     .then(response => response.json())
     //     .then(users => this.setState({robots: users}));
 
-    this.setState({robots: []});
+    this.setState({robotsGenerate: []});
+    // console.log(this.props.store.getState())
+
+    this.props.onRequestRobots();  
+    
 }
 
 // the long line
@@ -49,37 +74,43 @@ componentDidMount() {
 //          })
 // }
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value});
-    }
+//we can remove this now, since we already have Redux for it
+    // onSearchChange = (event) => {
+    //     this.setState({searchfield: event.target.value});
+    // }
 
     onClickGenerateRobots = (event) => {
         // console.log(event.target.parentElement.parentElement.children[2].children[0].value);
+        console.log(this.props.robots);
         event.target.parentElement.parentElement.children[2].children[0].value = '';
-        this.setState({searchfield:event.target.parentElement.parentElement.children[2].children[0].value });
+
+        //we don't set state manually - let's figure it out later
+        // this.setState({searchfield:event.target.parentElement.parentElement.children[2].children[0].value });
+        this.props.clearSearchField(event);
         if (event.target.parentElement.children[1].value > 200 ) {
             alert("Can't have more than 200 robots!");
             event.target.parentElement.children[1].value = 0;
-            this.setState({robots:[]})
+            this.setState({robotsGenerate:[]})
         } else {
-            this.setState({robots: generateRobots.generate(event.target.parentElement.children[1].value)});
+            this.setState({robotsGenerate: generateRobots.generate(event.target.parentElement.children[1].value)});
         }
     }
 
 
 
     render () {
-        const {robots, searchfield} = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase()); 
+        const {robotsGenerate} = this.state;
+        const {searchField, onSearchChange} = this.props;
+        const filteredRobots = robotsGenerate.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase()); 
         })
-
+        
 
         return (
             <div className="tc">
                 <h1>RoboFriends</h1>
                 <InputBox clickGenerate = {this.onClickGenerateRobots} checkRobot = {this.checkNumberofRobots}/>
-                <SearchBox searchChange = {this.onSearchChange} />
+                <SearchBox searchChange = {onSearchChange} />
                 <Scroll>
                     <ErrorBoundry>
                         <Cardlist robots={filteredRobots}  />
@@ -90,4 +121,4 @@ componentDidMount() {
             }
         }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
